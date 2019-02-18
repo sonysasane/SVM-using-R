50659ad5dfb03f880f3b15ba1d313d85401166f5
sonysasane <45103490+sonysasane@users.noreply.github.com>
2019-02-17 23:55:29 -0600

    Add files via upload

---
+++ svmClassifi.R
@@ -0,0 +1,31 @@
+df <- read.csv("Social_Network_Ads.csv")
+str(df)
+df <- df[,3:5]
+
+# converting the "purchased" column into factor
+
+df$Purchased <- factor(df$Purchased, levels = c(0,1))
+head(df)
+
+#splitting the data
+install.packages("caTools")
+library(caTools)
+
+split <- sample.split(df$Purchased, SplitRatio = 0.75)
+training_set <- subset(df,split == TRUE)
+test_set <- subset(df,split == FALSE)
+#classifier
+install.packages("e1071")
+library(e1071)
+
+classifier <- svm(formula = Purchased ~ .,
+                  data = training_set,
+                  type = "C-classification",
+                  kernel = "linear")
+
+y_pred <- predict(classifier,newdata = test_set[-3])
+
+# confucion matrix
+cm <- table(test_set[,3], y_pred)
+
+
