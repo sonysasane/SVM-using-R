b3338ac2c7ca7b09d08cd9a79c88e236baf2651c
sonysasane <45103490+sonysasane@users.noreply.github.com>
2019-02-19 15:22:14 -0600

    visualization code added

--- svmClassifi.R
+++ svmClassifi.R
@@ -43,7 +43,7 @@ contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
 points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
-# visualizing  test set 
+# visualizing  test set data
 
 set = test_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
